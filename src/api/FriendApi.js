d4062f6d21f24454fd73561b20e6ca2ef8d2c64b
harishtrv <61978004+harishtrv@users.noreply.github.com>
2022-02-20 08:20:09 +0530

    Update FriendApi.js

--- src/api/FriendApi.js
+++ src/api/FriendApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://harishtkottur.pythonanywhere.com'
+  baseURL: 'https://harishtkottur.pythonanywhere.com'
 });
 //http://127.0.0.1:5000/
 // http://harishtkottur.pythonanywhere.com/
