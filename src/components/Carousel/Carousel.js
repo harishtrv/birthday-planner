cecc8f9e8749f5373bb46d4f38be55e515496226
harisht26698 <harish.t@philips.com>
2022-03-03 16:17:31 +0530

    created carousel of app features

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from 'react';
+import FriendList from '../../assets/FriendList.PNG';
+import Card from '../../assets/Card.PNG';
+import Reminders from '../../assets/Reminders.PNG';
+import SuggestedFriendsList from '../../assets/SuggestedFriendsList.PNG';
+import AddConfirmButton from '../../assets/AddConfirmButton.PNG';
+import RemoveConfirmButton from '../../assets/RemoveConfirmButton.PNG';
+import styles from './carousel.module.css';
+import { BiArrowBack } from 'react-icons/bi';
+import { IoMdArrowForward } from 'react-icons/io';
+
+const Carousel = () => {
+  const [activeImgId, setActiveImgId] = useState(0);
+  useEffect(() => {
+    let intervalID = setInterval(() => {
+      setActiveImgId((activeImgId + 1) % 6);
+    }, 3500);
+    return () => clearInterval(intervalID);
+  });
+  const imageRenderer = () => {
+    switch (activeImgId) {
+      case 0:
+        return (
+          <div>
+            <img alt='List of Friends' className={styles.slider} src={FriendList} ></img>
+            <p>List of Friends</p>
+          </div>
+        );
+      case 1:
+        return (
+          <div>
+            <img alt='Friend Card' className={styles.slider} src={Card} ></img>
+            <p>Friend Card</p>
+          </div>
+        );
+        case 2:
+          return (
+            <div>
+              <img alt='Friends BirthDay Reminders' className={styles.slider} src={Reminders} ></img>
+              <p>Friends BirthDay Reminders</p>
+            </div>
+          );
+        case 3:
+        return (
+          <div>
+            <img alt='Suggested Friend List' className={styles.slider} src={SuggestedFriendsList} ></img>
+            <p>Suggested Friend List</p>
+          </div>
+        );
+        case 4:
+        return (
+          <div>
+            <img alt='Modal to Confirm Add Friend' className={styles.slider} src={AddConfirmButton} ></img>
+            <p>Modal to confirm adding new friend</p>
+          </div>
+        );
+        case 5:
+        return (
+          <div>
+            <img alt='Confirm Remove Friend'className={styles.slider} src={RemoveConfirmButton} ></img>
+            <p>Remove friend confirmation</p>
+          </div>
+        );
+      default:
+        return (
+          <div>m</div>
+        );
+    }
+  }
+  const goBack = () =>{
+    if(activeImgId === 0){
+      setActiveImgId(5);
+    }
+    else{
+      setActiveImgId(activeImgId-1);
+    }
+  }
+  const goForward = () => {
+    setActiveImgId((activeImgId+1)%6);
+  }
+  return (
+    <div className={styles.container}>
+      <h3>Signup to explore more</h3>
+      {imageRenderer()}
+      <div onClick={goBack} className={styles.back}><BiArrowBack /></div>
+      <div onClick={goForward} className={styles.forward}><IoMdArrowForward /></div>
+      
+    </div>
+  );
+}
+
+export default Carousel;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,30 +4,44 @@ import history from '../../history';
 import SignUp from '../SignUp/SignUp';
 import SignIn from '../SignIn/SignIn';
 import styles from './home.module.css';
+import Carousel from '../Carousel/Carousel';
 
 class Home extends React.Component {
-  state = { buttonStatus: false}
+  state = { buttonStatus: false }
   validateAndRender() {
     if (this.props.isSignedIn) {
       history.push('/cards');
       return <div></div>;
     }
     return <div className={styles.container2}>
-      <button className={styles.btn} onClick={()=>this.setState({buttonStatus: !this.state.buttonStatus})}>{this.state.buttonStatus ? 'Click here for SignIn' : 'Click here for SignUp'}</button>
+      <div className={styles.selection}>
+        <button className={styles.btn}
+          onClick={() => this.setState({ buttonStatus: true })}>
+          SignUp
+        </button>
+        <button className={styles.btn}
+          onClick={() => this.setState({ buttonStatus: false })}>
+          SignIn
+        </button>
+      </div>
+      <hr className={this.state.buttonStatus ? styles.signUphr : styles.signInhr} />
       {this.state.buttonStatus ? <SignUp /> : <SignIn />}
     </div>;
   }
   render() {
     return (
-      <div className={styles.container}>
+      <div>
         <h1>Welcome to Birthday Planner</h1>
-        {this.validateAndRender()}
+        <Carousel />
+        <div className={styles.container}>
+          {this.validateAndRender()}
+        </div>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => {
-  return { isSignedIn: state.auth.isSignedIn || state.user.isSignedIn};
+  return { isSignedIn: state.auth.isSignedIn || state.user.isSignedIn };
 }
 export default connect(mapStateToProps, null)(Home);
