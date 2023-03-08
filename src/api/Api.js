e383886865e1503d730d3b085dba57e43ddb0708
harishtrv <61978004+harishtrv@users.noreply.github.com>
2023-03-08 15:52:39 +0530

    Update Api.js

--- src/api/Api.js
+++ src/api/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
 
-  baseURL: 'http://127.0.0.1:5000'
+  baseURL: 'https://harishtkottur.pythonanywhere.com'
 });
 //http://127.0.0.1:5000
 // https://harishtkottur.pythonanywhere.com
